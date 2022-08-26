b47d12e662eb11a781b55d72f26946ac3a2a2d66
Xavier Dyer <xdyerm@gmail.com>
2022-08-26 13:58:12 -0500

    First commit

---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// const Card = (props) => {
+//     return (
+//         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'>
+//             <img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
+//             <div>
+//                 <h2>  {props.name} </h2>
+//                 <p> {props.email}</p>
+//             </div>
+//         </div>
+//     );
+// }
+
+// const Card = (props) => {
+//     const {name, email, id} = props;
+//     return (
+//         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'>
+//             <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+//             <div>
+//                 <h2>  {name} </h2>
+//                 <p> {email}</p>
+//             </div>
+//         </div>
+//     );
+// }
+
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+            <div>
+                <h2>  {name} </h2>
+                <p> {email}</p>
+            </div>
+        </div>
+    );
+}
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,25 @@
+export const robots = [{
+    id: 1,
+    name: 'Xavier Dyer Montoya',
+    username: 'xavier',
+    email: 'xdyerm@gmail.com'
+},
+{
+    id: 2,
+    name: 'Sandy Viviana Gaona Ortega',
+    username: 'sandy',
+    email: 'sandy@hotmail.com'
+},
+{
+    id: 3,
+    name: 'Anastasia Jimena Lorenzetti González',
+    username: 'anastasia',
+    email: 'anastasia@hotmail.com'
+},
+{
+    id: 4,
+    name: 'Benancio Aparicio Diocesano',
+    username: 'Benny',
+    email: 'benny@hotmail.com'
+}
+]
